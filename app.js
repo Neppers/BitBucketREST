459f6ede82081bb9700dc98a5e63fa4b769c6838
Nick Price <neppers@gmail.com>
2014-03-16 15:19:00 +0000

    Minor correction

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express'),
     http = require('http'),
     httpProxy = require('http-proxy');
 
-var lastfmProxy = httpProxy.createServer({
-    target: 'https://bitbucket.org/'
+var bbProxy = httpProxy.createServer({
+    target: 'https://bitbucket.org/plans'
 }).listen(3002, function() {
     console.log('Proxy listening on 3002');
 });
